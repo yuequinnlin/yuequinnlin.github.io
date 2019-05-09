a710a8a23ac3900e43f7c2f39fb87cca550fadc0
Quinn <yuequinnlin@gmail.com>
2019-05-09 18:43:52 -0500

    test

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function init() {
+   console.log("Quinn >>>>>>>>>>>>>>>");
    $.getJSON("discs.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
-});
+       console.log(json); // this will show the info it in firebug console
+   });
 }
