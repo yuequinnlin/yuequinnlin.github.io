73a4b1695b3067b298755cce62978dfbde55c325
Quinn <yuequinnlin@gmail.com>
2019-05-09 18:58:47 -0500

    discs json

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function init() {
    console.log("Quinn >>>>>>>>>>>>>>>")
-   $.getJSON("../discs.json", function(json) {
+   $.getJSON("/discs.json", function(json) {
        console.log(json); // this will show the info it in firebug console
    });
+
+   var mydata = JSON.parse(data);
 }
